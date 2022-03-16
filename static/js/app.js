6ff57f0e81583392e2c9853142fb876a654f54e2
King David <davidwiers@davids-mbp.lan>
2022-03-16 14:56:10 -0500

    adding recent file updates

--- static/js/app.js
+++ static/js/app.js
@@ -24,23 +24,35 @@ function buildTable(data) {
 }
 
 // 1. Create a variable to keep track of all the filters as an object.
-var filters;
+let filters = {city, state, country, shape, duration};
+var city;
+var state;
+var country;
+var shape;
+var duration;
+
 
 
 // 3. Use this function to update the filters. 
 function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
-
+ let changedElement = d3.select(this);
     // 4b. Save the value that was changed as a variable.
-
+let elementValue = changedElement.property("value")
+console.log(elementValue);
     // 4c. Save the id of the filter that was changed as a variable.
-
+let filterId = changedElement.attr("id")
+console.log(filterId);
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
- 
-  
+ if (elementValue != " ") {
+    filters[filterId] = elememtValue;
+ }
+  else {
+    delete filters[filterId]
+  }
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
   
@@ -54,7 +66,8 @@ function updateFilters() {
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    
+    // 9. Loop through all of the filters and keep any data that matches the filter values
+
   
     // 10. Finally, rebuild the table using the filtered data
     
